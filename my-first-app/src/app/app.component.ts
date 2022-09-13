e599646420e61775b7c8ec692322c858425a2e92
Febin George (BLR PDC) <febing@nousinfo.com>
2022-09-13 10:55:50 +0530

    With some changes

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css'],
   styles: [
     `
       h1 {
@@ -13,5 +13,11 @@ import { Component } from '@angular/core';
   ],
 })
 export class AppComponent {
+  showSecret = false;
   name = 'my-first-app';
+  log = [];
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -12,6 +12,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server is created';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Testserveer', 'Terstserver 2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -24,6 +26,8 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus =
       'Server was created! Name is ' + this.serverName;
   }
