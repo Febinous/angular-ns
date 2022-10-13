0897a5367a062cf51fda472fd5f8da0706d22cbd
Febin George (BLR PDC) <febing@nousinfo.com>
2022-10-13 16:50:50 +0530

    comit

--- TryOut/Routing/routing-start/routing-start/src/app/users/user/user.component.ts
+++ TryOut/Routing/routing-start/routing-start/src/app/users/user/user.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"],
 })
 export class UserComponent implements OnInit {
-  user: {id: number, name: string};
-
-  constructor() { }
+  user: { id: number; name: string };
+  paramsSub: Subscription;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params["id"],
+      name: this.route.snapshot.params["name"],
+    };
+    this.paramsSub = this.route.params.subscribe((data) => {
+      this.user.id = data["id"];
+      this.user.name = data["name"];
+    });
   }
-
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+  }
 }
